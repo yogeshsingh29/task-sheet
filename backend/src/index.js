e53c45d1e3d60e01919854b0b0b9cb8114b8ccae
yogeshsingh29 <yogeshsingh0095@gmail.com>
2025-04-28 23:31:08 +0530

    cors updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,10 @@ dotenv.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 app.use(express.json());
 
 
